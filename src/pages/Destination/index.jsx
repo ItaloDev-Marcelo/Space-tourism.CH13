abc0ff7a73ce659b239aacd8b7966281b8ca8ea2
Italo Marcelo <devfrontend2028study@gmail.com>
2025-02-18 15:37:11 -0300

    update destination section

--- src/pages/Destination/index.jsx
+++ src/pages/Destination/index.jsx
@@ -3,9 +3,8 @@ import Navbar from '../../components/Nav/Navbar';
 export default function Destination()  {
     return (
         <section className='banner-Destination '>
-             
             <Navbar whereTo0={'link'} whereTo1={'link active'} whereTo2={'link'} whereTo3={'link'}  />
-            <h1>Destination page</h1>
+            <h1> <span className='space-num'>01</span> <span className='space-title'>PICK YOUR DESTINATION</span> </h1>
         </section>
     )
 }
