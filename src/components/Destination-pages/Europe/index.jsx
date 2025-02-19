bc3f1380c112022ae1bca96de1608e435428906d
Italo Marcelo <devfrontend2028study@gmail.com>
2025-02-19 16:43:47 -0300

    up

--- src/components/Destination-pages/Europe/index.jsx
+++ src/components/Destination-pages/Europe/index.jsx
@@ -1,25 +1,15 @@
 
 import {Box, Typography} from '@mui/material'
-import Europe from '../../../assets/assets/destination/image-mars.webp';
+import Europe from '../../../assets/assets/destination/image-europa.webp';
 
-import { useNavigate } from 'react-router-dom';
 export default function EuropePage() {
 
-  const navigate  = useNavigate()
       return  (
             <Box className='box--Container'>
             <Box className='planet-container'>
                 <img src={Europe} alt='europe' />
             </Box>
             <Box className='planet-content' component='section'>
-            <nav className='tabBar'>
-                 <ul>
-                    <li  onClick={() => navigate('/moon')}><a href=''>MOON</a></li>
-                    <li  onClick={() => navigate('/mars')}><a href=''>MARS</a></li>
-                    <li onClick={() => navigate('/europe')}><a href=''>EUROPE</a></li>
-                    <li  onClick={() => navigate('/titan')}><a href=''>TITAN</a></li>
-                 </ul>
-              </nav>
                 <Box component='article'>
                   <Typography component='h2' className='planet-title'></Typography>
                   <Typography component='p'></Typography>
--- src/pages/Destination/index.jsx
+++ src/pages/Destination/index.jsx
@@ -1,18 +1,41 @@
 
+import { useState } from 'react';
 import EuropePage from '../../components/Destination-pages/Europe';
 import MarsPage from '../../components/Destination-pages/Mars';
 import MoonPage from '../../components/Destination-pages/Moon';
 import TitansPage from '../../components/Destination-pages/Titans';
 import Navbar from '../../components/Nav/Navbar';
 
-import { Navigate, Route, Routes } from "react-router-dom"
 export default function Destination()  {
+
+    const [destination, setDestination]  =  useState('moon') 
+
+    const nav = (i) => {
+        setDestination(i)
+    }
+
+    console.log(destination)
 
     return (
         <section className='banner-Destination '>
             <Navbar whereTo0={'link'} whereTo1={'link active'} whereTo2={'link'} whereTo3={'link'}  />
-            
-   
+            <nav className='tabBar'>
+                 <ul>
+                    <li className={destination === 'moon'  ? 'tab-link active' : 'tab-link' }  onClick={() => nav('moon')}><a href='#'>MOON</a></li>
+                    <li className={destination === 'mars'  ? 'tab-link active' : 'tab-link' }  onClick={() => nav('mars')}><a href='#'>MARS</a></li>
+                    <li className={destination === 'europe'  ? 'tab-link active' : 'tab-link' }  onClick={() => nav('europe')}><a href='#'>EUROPE</a></li>
+                    <li className={destination === 'titan'  ? 'tab-link active' : 'tab-link' }  onClick={() => nav('titan')}><a href='#'>TITAN</a></li>
+                 </ul>
+              </nav>
+
+             {
+                destination === 'moon' ? <MoonPage/> :
+                destination === 'mars' ? <MarsPage/> :
+                destination === 'europe' ? <EuropePage/> :
+                destination === 'titan' ? <TitansPage/> : null
+             }
+
+
         </section>
     )
 }
