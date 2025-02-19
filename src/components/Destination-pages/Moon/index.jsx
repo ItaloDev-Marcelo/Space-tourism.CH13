50bfd5349fab3a7e3eacdb2bc0a096e300f65dc3
Italo Marcelo <devfrontend2028study@gmail.com>
2025-02-19 16:46:12 -0300

    update

--- src/components/Destination-pages/Moon/index.jsx
+++ src/components/Destination-pages/Moon/index.jsx
@@ -12,13 +12,19 @@ export default function MoonPage() {
           <Box className='planet-content' component='section'>
     
               <Box component='article'>
-                <Typography component='h2' className='planet-title'></Typography>
-                <Typography component='p'></Typography>
+                <Typography component='h2' className='planet-title'> Moon</Typography>
+                <Typography component='p'>See our planet as you’ve never seen it before. A perfect relaxing trip away to help 
+  regain perspective and come back refreshed. While you’re there, take in some history 
+  by visiting the Luna 2 and Apollo 11 landing sites.</Typography>
                 <Box className='normal-line'></Box>
                 <Box className='row-infomation'>
                    <Box className='item'>
-                   <Typography component='h3' className='item-title'></Typography> 
-                   <Typography component='h4' className='item-subtitle'></Typography>
+                   <Typography component='h3' className='item-title'>Avg. distance</Typography> 
+                   <Typography component='h4' className='item-subtitle'>384,400 km</Typography>
+                   </Box>
+                   <Box className='item'>
+                   <Typography component='h3' className='item-title'>Est. travel time</Typography> 
+                   <Typography component='h4' className='item-subtitle'> 3 days</Typography>
                    </Box>
                 </Box>
               </Box>
